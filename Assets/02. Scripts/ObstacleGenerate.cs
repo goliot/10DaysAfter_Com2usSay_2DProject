a6591d1fb35c91cbd83f8d91ea38fa228e3548bb
Sumin Cha <cktnals2000@naver.com>
2026-01-07 23:58:58 +0900

    BeforeBuild

--- Assets/02. Scripts/ObstacleGenerate.cs
+++ Assets/02. Scripts/ObstacleGenerate.cs
@@ -1,5 +1,7 @@
-using UnityEditor.SceneManagement;
+#if UNITY_EDITOR
 using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
 using UnityEngine;
 
 public class ObstacleGenerate : MonoBehaviour
@@ -15,19 +17,33 @@ public class ObstacleGenerate : MonoBehaviour
         // 기존 자식 제거
         for (int i = transform.childCount - 1; i >= 0; i--)
         {
-            DestroyImmediate(transform.GetChild(i).gameObject);
+#if UNITY_EDITOR
+            // Undo 되게 하려면 DestroyImmediate 대신 이걸 쓰는 게 좋아요
+            Undo.DestroyObjectImmediate(transform.GetChild(i).gameObject);
+#else
+            Destroy(transform.GetChild(i).gameObject);
+#endif
         }
 
 #if UNITY_EDITOR
         Undo.RegisterFullObjectHierarchyUndo(this.gameObject, "Clear Old Obstacles");
 #endif
 
+        if (obstaclePrefabs == null || obstaclePrefabs.Length == 0) return;
+
         for (int i = 0; i < count; i++)
         {
             GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
-            GameObject obstacle = (GameObject)GameObject.Instantiate(obstaclePrefab);
+            if (obstaclePrefab == null) continue;
 
-            obstacle.transform.SetParent(transform);
+            GameObject obstacle;
+#if UNITY_EDITOR
+            obstacle = (GameObject)PrefabUtility.InstantiatePrefab(obstaclePrefab);
+            Undo.RegisterCreatedObjectUndo(obstacle, "Spawn Obstacle");
+#else
+            obstacle = Instantiate(obstaclePrefab);
+#endif
+            obstacle.transform.SetParent(transform, true);
 
             float angle = Random.Range(0f, 2 * Mathf.PI);
             float t = Random.Range(0f, 1f);
@@ -35,14 +51,10 @@ public class ObstacleGenerate : MonoBehaviour
             Vector3 pos = new Vector3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0);
 
             obstacle.transform.position = transform.position + pos;
-
-#if UNITY_EDITOR
-            Undo.RegisterCreatedObjectUndo(obstacle, "Spawn Obstacle");
-#endif
         }
 
 #if UNITY_EDITOR
-        EditorUtility.SetDirty(this.gameObject); // 변경 플래그 설정
+        EditorUtility.SetDirty(this.gameObject);
         EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
 #endif
     }
